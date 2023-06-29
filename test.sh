7587e86f9e27d2a2be6ec3e5e52bc5116921b532
Daniel Schmidt <dcschmidt@berkeley.edu>
2023-06-29 12:53:36 -0700

    Configures a GitHub Action to test the backup container

--- test.sh
+++ test.sh
@@ -1,32 +1,34 @@
 #!/bin/sh -e
 
 function cleanup {
-    docker compose down -v
+    echo "Cleaning up..."
+    docker compose down -v > /dev/null 2>&1
 }
 trap cleanup EXIT
 
-function existing_snapshots() {
+function solr_snapshots {
     docker compose exec solr find /var/solr/data -name 'snapshot.*'
 }
 
 echo "Preparing the test stack..."
-docker compose build
-docker compose up -d solr
-docker compose exec solr wait-for-solr.sh
+docker compose build > /dev/null 2>&1
+docker compose up -d solr > /dev/null 2>&1
+docker compose exec solr wait-for-solr.sh > /dev/null 2>&1
 
 echo "Verifying that solr has no existing backups..."
-if [[ "$(existing_snapshots)" ]]; then
+if [ "$(solr_snapshots | wc -l)" -ne "0" ]; then
     echo "ERROR: Solr already has backups. Aborting."
     exit 1
 fi
 
+echo "Running the backup container..."
 docker compose run --rm app
 sleep 2
 
-if [[ ! "$(existing_snapshots)" ]]; then
-    echo "FAILURE: Solr core was not backed up."
-    exit 1
-else
-    echo "SUCCESS: Solr core was backed up."
+if [ "$(solr_snapshots | wc -l)" -eq "2" ]; then
+    echo "SUCCESS: All Solr cores were backed up."
     exit 0
+else
+    echo "FAILURE: All Solr cores were not backed up."
+    exit 1
 fi
